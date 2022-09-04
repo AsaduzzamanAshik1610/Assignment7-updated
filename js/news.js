8443eb8207a499763eb9278a073fe81868a8eab9
Asaduzzaman Ashik1610 <mdashik1610@gmail.com>
2022-09-04 11:01:54 +0600

    added the acordino

--- js/news.js
+++ js/news.js
@@ -8,7 +8,7 @@ const newsPortel = () => {
 const displayNewsportel = News => {
     const newsContainer = document.getElementById('news-container');
     News.forEach(portal => {
-        console.log(portal.category_name);
+        // console.log(portal.category_name);
         const newsli = document.createElement('li');
         newsli.onclick = () => displayCatagory(portal.category_id)
         newsli.innerHTML = `
@@ -19,7 +19,7 @@ const displayNewsportel = News => {
 
 }
 const displayCatagory = id => {
-    console.log(id);
+    // console.log(id);
     const url = ` https://openapi.programming-hero.com/api/news/category/${id}`
     fetch(url)
         .then(res => res.json())
@@ -31,8 +31,9 @@ const displayCatagory = id => {
 const displayFasion = fashions => {
     const fashionContainer = document.getElementById('fashion-container');
     fashionContainer.innerHTML = '';
+    fashions.sort((a, b) => b.total_view - a.total_view)
     fashions.forEach(fashion => {
-        console.log(fashion)
+        // console.log(fashion._id)
         const fashionDiv = document.createElement('div');
         // fashionDiv.classList.add('row', 'gap-3');
         fashionDiv.innerHTML = `
@@ -44,9 +45,11 @@ const displayFasion = fashions => {
         <h5 class="card-title">${fashion.title}</h5>
         <p class="card-text">${fashion.details}</p>
         <img class="autor-images" src="${fashion.author.img}" alt="">
-        <i class="fa-thin fa-eye">${fashion.total_view}</i> 
-        <button onclick="newsDetails('${fashion}') " href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newsDetailModal">show detail</button>
-        </button>    
+        <span> ${fashion.author.name} </span>
+        <i class="fa-solid fa-eye"></i>
+        <span>${fashion.total_view}</span> 
+        <button onclick="newsDetails('${fashion._id}')" href="#" class="btn btn-primary"data-bs-toggle="modal" data-bs-target="#exampleModal">show detail</button>
+        </button> 
         </div>
         </div>
        `;
@@ -55,10 +58,32 @@ const displayFasion = fashions => {
     })
 }
 const newsDetails = id => {
-    const url = `https://openapi.programming-hero.com/api/news/category/${id}`
+    const url = `https://openapi.programming-hero.com/api/news/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaynewDetails(data.data[0]));
+}
+
+const displaynewDetails = news => {
+    console.log(news);
+    const newsDetail = document.getElementById('newsDetailModal');
+    newsDetail.innerHTML = `
+    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">${news.title}</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                        <img class="img-fluid" src="${news.image_url}" alt="">
+                        <p class="py-3">${news.details}</p>  
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            
+                        </div>
+                    </div>
+    `
+
 }
 
 newsPortel();
